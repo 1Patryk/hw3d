7786686fb44f60dc9d155c0a8f18508c4bd74c9c
Patrick <1patryk.polak@gmail.com>
2024-02-29 19:46:35 +0100

    Video nr 7

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,31 +1,4 @@
-#include <Windows.h>
-#include <sstream>
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(1);
-		break;
-
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Respects");
-		}
-		break;
-	case WM_LBUTTONDOWN:
-		{
-			POINTS pt = MAKEPOINTS(lParam);
-			std::ostringstream oss;
-			oss << "(" << pt.x << "," << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-		}
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Window.hpp"
 
 int CALLBACK WinMain(
 		HINSTANCE hInstance,
@@ -33,44 +6,12 @@ int CALLBACK WinMain(
 		LPSTR	  lpCmdLine,
 		int		  nCmdShow)
 {
-	//register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = "3Dclass";
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
+	Window wnd1(800, 300, "Fart Box 1");
+	Window wnd2(300, 800, "Fart Box 2");
 
-	// create window instance
-	HWND hWnd = CreateWindowExW(
-		0,
-		L"3Dclass",
-		L"First Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200,
-		200,
-		640,
-		480,
-		nullptr,
-		nullptr,
-		hInstance,
-		nullptr
-	);
-
-	ShowWindow(hWnd, SW_SHOW);
-	
-	// message 
 	MSG msg;
 	BOOL gResult;
-	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
@@ -80,13 +21,9 @@ int CALLBACK WinMain(
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
 
-	return 0;
+	return msg.wParam;
 }
 
+// next: https://www.youtube.com/watch?v=QYGLXhulvVQ&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=8&ab_channel=ChiliTomatoNoodle
 
-// 5 lesson next https://www.youtube.com/watch?v=UUbXK4G_NCM&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=5&ab_channel=ChiliTomatoNoodle
